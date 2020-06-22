3d071c020bc9bd4ceb261aa5dcf0ffa9ac22c939
katguerin <katguerin55@gmail.com>
2020-06-22 15:25:24 -0700

    currently displaying at the port. yay!

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ const express = require("express");
 const app = express();
 const homeRoutes = require('./home-routes.js');
 const router = require('express').Router();
-
 router.use('/', homeRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,5 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// added below during zoom with helper
+app.use(routes);
 app.listen(PORT, () => console.log(`LISTENING ON PORT: ${PORT}`));
